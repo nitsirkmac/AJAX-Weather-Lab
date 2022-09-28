d56435ddfe390dcdc7784abe04316359f616a5a7
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-09-28 00:51:26 -0400

    pulling info from event listener

--- js/script.js
+++ js/script.js
@@ -8,12 +8,20 @@ document.querySelector('input').placeholder = "City Name";
 const getInfoBtn = document.querySelector('button');
 const inputBox = document.querySelector('input');
 
+
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox}&appid=c0332f214fd1e9a8b4334f5909bf6fbf`
 
 
 getInfoBtn.addEventListener('click', (e) => {
     e.preventDefault()
     console.log(inputBox.value)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputBox}&appid=c0332f214fd1e9a8b4334f5909bf6fbf`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q= ${inputBox.value} &appid=c0332f214fd1e9a8b4334f5909bf6fbf`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
     
 })
+
+// const locale = document.getElementByID('#local');
+// 
