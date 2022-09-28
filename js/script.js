4b29e334c4cb78371662401262669931a91e3881
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-09-28 00:18:32 -0400

    main content-save.1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// uses search form to show weather in any city the user searches for
+
+
+//API Key: c0332f214fd1e9a8b4334f5909bf6fbf
+
+document.querySelector('input').placeholder = "City Name";
+
+const getInfoBtn = document.querySelector('button');
+const inputBox = document.querySelector('input');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox}&appid=c0332f214fd1e9a8b4334f5909bf6fbf`
+
+
+getInfoBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(inputBox.value)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputBox}&appid=c0332f214fd1e9a8b4334f5909bf6fbf`)
+    
+})
