183b537d42bdbeb594f96445756cb0d000c6d6e3
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-05 15:54:03 -0400

    Attempting to populate data

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@
 
 //API Key: c0332f214fd1e9a8b4334f5909bf6fbf
 
+
 document.querySelector('input').placeholder = "City Name";
 
 const getInfoBtn = document.querySelector('button');
 const inputBox = document.querySelector('input');
-
+const locale = document.getElementById('local');
+const temp = document.getElementById('temp');
+const feel = document.getElementById('feels');
+const weather = document.getElementById('current');
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox}&appid=c0332f214fd1e9a8b4334f5909bf6fbf`
 
@@ -15,13 +19,24 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox}&appid
 getInfoBtn.addEventListener('click', (e) => {
     e.preventDefault()
     console.log(inputBox.value)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q= ${inputBox.value} &appid=c0332f214fd1e9a8b4334f5909bf6fbf`)
+    fetch(url)
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
+        locale.innerText = data.name
+        // temp.innerText = data.main.temp 
+        // feel.innerText = data.main.feels_like
+        // weather.innerText = data.weather[0].description
+        
     })
-    
+
 })
 
-// const locale = document.getElementByID('#local');
-// 
+// fetch(url).then(res => {
+//     console.log('success!', res)
+// }).catch(err => {
+//     console.log('something went wrong', err)
+// })
+
+// temp = ((data.main.temp * 9/5) + 32) 
+        // feels = ((data.main.feels_like * 9/5) + 32)
