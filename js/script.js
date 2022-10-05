1eed66feab7539f3d0e50b0b3fd813d1a2271a17
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-05 16:30:56 -0400

    Weather App Complete

--- js/script.js
+++ js/script.js
@@ -13,20 +13,20 @@ const temp = document.getElementById('temp');
 const feel = document.getElementById('feels');
 const weather = document.getElementById('current');
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&appid=c0332f214fd1e9a8b4334f5909bf6fbf`
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&units=imperial&appid=c0332f214fd1e9a8b4334f5909bf6fbf`
 
 
 getInfoBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&appid=c0332f214fd1e9a8b4334f5909bf6fbf`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&units=imperial&appid=c0332f214fd1e9a8b4334f5909bf6fbf`
     console.log(inputBox.value)
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
         locale.innerText = `Weather for: ${data.name}`
-        temp.innerText = `Temperature: ${data.main.temp}` 
-        feel.innerText = `Feels Like: ${data.main.feels_like}`
+        temp.innerText = `Temperature: ${data.main.temp} \u00B0F` 
+        feel.innerText = `Feels Like: ${data.main.feels_like} \u00B0F`
         weather.innerText = `Current Conditions: ${data.weather[0].description}`
         
     })
